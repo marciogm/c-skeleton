8bd23ac29de5c12447fc586c722e77a5dd7daa08
marciogm <marciogiaxa@gmail.com>
2012-04-26 23:26:11 -0300

    Run all tests before showing output results and a better visualization

--- tests/minunit.h
+++ tests/minunit.h
@@ -8,26 +8,27 @@
 
 #define mu_suite_start() char *message = NULL
 
+#define mu_tested(test, message) do { printf("\n%-10s : %s",test, message); } while(0)
 #define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
-#define mu_run_test(test) debug("\n-----%s", " " #test);\
-    message = test(); tests_run++; if (message) return message;
+#define mu_run_test(test) do { message = test(); tests_run++; if (message) { debug("\n-----%s", " " #test);\
+    tests_failed++; mu_tested(#test, message); } else { tests_passed++; } } while(0)
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
     argc = 1;\
-    debug("----- RUNNING: %s", argv[0]);\
     printf("----\nRUNNING: %s\n", argv[0]);\
-    char *result = name();\
-    if (result != 0) {\
-        printf("FAILED: %s\n", result);\
+    name();\
+    if (!(tests_failed)) {\
+        printf("\n\nALL TESTS PASSED\n");\
     }\
-    else {\
-        printf("ALL TESTS PASSED\n");\
-    }\
-    printf("Tests run: %d\n", tests_run);\
-    exit(result != 0);\
+    printf("\n\nTests run: %d", tests_run);\
+    printf("\nTests passed: %d", tests_passed);\
+    printf("\nTests failed: %d\n\n", tests_failed);\
+    exit(tests_failed);\
 }
 
 
 int tests_run;
+int tests_failed;
+int tests_passed;
 
 #endif
